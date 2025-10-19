e1e53bddd91fea744e6edfe85f74cf4e977e5ff0
tqer39 <tqer39@gmail.com>
2025-10-20 08:02:16 +0900

    ✨ ドメイン登録の作成を有効化する設定を更新

--- infra/terraform/envs/prod/domain/main.tf
+++ infra/terraform/envs/prod/domain/main.tf
@@ -16,7 +16,7 @@ module "domain_register_delegate" {
   yearly_price_currency = var.yearly_price_currency
   yearly_price_units    = var.yearly_price_units
 
-  create_domain_registration = false
+  create_domain_registration = true
 }
 
 # 有効化順の依存（保守的に明示）
